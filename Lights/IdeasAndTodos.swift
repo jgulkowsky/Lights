dd8a5da513dff3969fc336710bd6cfdda03ac138
Jan Gulkowski <jan.gulkowski@ihsmarkit.com>
2021-04-23 01:47:16 +0200

    Create IdeasAndTodos file to store them in one place (of course it is only about todos that are loosely coupled to file they are in - like here ides in VM)

---
+++ Lights/IdeasAndTodos.swift
@@ -0,0 +1,21 @@
+//
+//  IdeasAndTodos.swift
+//  Lights
+//
+//  Created by Jan Gulkowski on 23/04/2021.
+//
+
+import Foundation
+
+//todo: same ideas (almost) but more precise:
+//todo: tapping on PLAY button switches mode to playing
+//todo: tapping on PAUSE button switches mode to paused
+//todo: when in paused mode the color is constant until user changes it manualy
+//todo: when in plauing mode the color changes with transition
+//todo: in playing mode user can change speed of changes with speedometer on the right hand side
+//todo: normally ui is invisible to activate it user needs to tap (it doesn't change color in such situation)
+//todo: in playing mode user will see PAUSE button to switch mode and speedometer
+//todo: in paused mode user will see only PLAY button to switch mode
+//todo: when ui is visible and user taps on screen the color is changed to some other random one (it's true for both modes)
+
+//todo: maybe add chameleon mode -> so you can open invisible camera and telephone will change color to what's behind it?? (if possible to open camera in quiet mode)
--- Lights/VMs/ViewModel.swift
+++ Lights/VMs/ViewModel.swift
@@ -46,15 +46,4 @@ class ViewModel {
         //todo: ui visible
         //todo: after time ui invisible -> we need to store this state too
     }
-    
-    //todo: same ideas (almost) but more precise:
-    //todo: tapping on PLAY button switches mode to playing
-    //todo: tapping on PAUSE button switches mode to paused
-    //todo: when in paused mode the color is constant until user changes it manualy
-    //todo: when in plauing mode the color changes with transition
-    //todo: in playing mode user can change speed of changes with speedometer on the right hand side
-    //todo: normally ui is invisible to activate it user needs to tap (it doesn't change color in such situation)
-    //todo: in playing mode user will see PAUSE button to switch mode and speedometer
-    //todo: in paused mode user will see only PLAY button to switch mode
-    //todo: when ui is visible and user taps on screen the color is changed to some other random one (it's true for both modes)
 }
