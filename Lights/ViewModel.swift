8253ba38aa0ee2f9507d0253a609c88a8ed92dc7
Jan Gulkowski <jan.gulkowski@ihsmarkit.com>
2021-04-23 00:27:52 +0200

    Add todos

--- Lights/ViewModel.swift
+++ Lights/ViewModel.swift
@@ -19,4 +19,10 @@ class ViewModel {
     private func chooseNewScreenColor() {
         screenColor.accept(UIColor.random)
     }
+    
+    //todo: some ideas
+    //todo: add play / pause button (only one and it has two modes) on the bottom of the screen
+    //todo: in play mode colors will change automatically with some animation
+    //todo: in pause mode color will stay until user taps on screen (or it can ficker a little bit but in the same color)
+    //todo: in play mode when user taps on screen he will be shown ui elements like pause button and speedometer (on the right) where he can set up speed of the animation
 }
